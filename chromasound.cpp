cfcfaa48529ec3a25d7d52db9cf6b8a2647ec5b4
Nir Jacobson <nirjacobson@gmail.com>
2024-11-06 14:22:12 -0500

    update fuse settings

--- chromasound.cpp
+++ chromasound.cpp
@@ -3,9 +3,9 @@
 const QUrl Chromasound::FirmwareURL("https://github.com/nirjacobson/releases/raw/refs/heads/master/Chromasound/firmware/");
 
 const Chromasound Chromasound::ChromasoundNova(128, false, "csnova.bin", QByteArray("\x1e\x95\x16", 3), 0xEE, 0xD8, 0xF7);
-const Chromasound Chromasound::ChromasoundNovaDirect(128, true, "csnova-direct.bin", QByteArray("\x1e\x95\x16", 3), 0xEE, 0xD8, 0xF7);
-const Chromasound Chromasound::ChromasoundPro(256, false, "cspro.bin", QByteArray("\x1e\x96\x0a", 3), 0xE6, 0xD8, 0xFF);
-const Chromasound Chromasound::ChromasoundProDirect(256, true, "cspro-direct.bin", QByteArray("\x1e\x96\x0a", 3), 0xE6, 0xD8, 0xFF);
+const Chromasound Chromasound::ChromasoundNovaDirect(128, true, "csnova-direct.bin", QByteArray("\x1e\x95\x16", 3), 0xEE, 0xDA, 0xF7);
+const Chromasound Chromasound::ChromasoundPro(256, false, "cspro.bin", QByteArray("\x1e\x96\x0a", 3), 0xE6, 0xDA, 0xFF);
+const Chromasound Chromasound::ChromasoundProDirect(256, true, "cspro-direct.bin", QByteArray("\x1e\x96\x0a", 3), 0xE6, 0xDC, 0xFF);
 
 int Chromasound::pageSize() const
 {
