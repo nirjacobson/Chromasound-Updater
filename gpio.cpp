ea5aa50b5c74f786420e738116f55a3113b799e4
Nir Jacobson <nirjacobson@gmail.com>
2024-11-15 19:52:09 -0500

    revert GPIO code

--- gpio.cpp
+++ gpio.cpp
@@ -27,24 +27,21 @@ void GPIO::write(int pin, int value)
 {
     int ret;
 
-    struct gpio_v2_line_request rq;
-    memset(&rq, 0, sizeof(rq));
+    struct gpiohandle_request rq;
+    rq.lineoffsets[0] = pin;
+    rq.lines = 1;
+    rq.flags = GPIOHANDLE_REQUEST_OUTPUT;
 
-    rq.offsets[0] = pin;
-    rq.num_lines = 1;
-    rq.config.flags = GPIO_V2_LINE_FLAG_OUTPUT;
-
-    ret = ioctl(_fd, GPIO_V2_GET_LINE_IOCTL, &rq);
+    ret = ioctl(_fd, GPIO_GET_LINEHANDLE_IOCTL, &rq);
 
     if (ret == -1) {
         printf("Unable to get line handle from ioctl: %s", strerror(errno));
         return;
     }
 
-    struct gpio_v2_line_values data;
-    data.bits = value;
-    data.mask = 1;
-    ret = ioctl(rq.fd, GPIO_V2_LINE_SET_VALUES_IOCTL, &data);
+    struct gpiohandle_data data;
+    data.values[0] = value;
+    ret = ioctl(rq.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
 
     if (ret == -1) {
         printf("Unable to set line value using ioctl: %s", strerror(errno));
